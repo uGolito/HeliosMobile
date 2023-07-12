19aa86897f4cecc59f411edba00fcaf2f4f21ea8
Closus <claudio@manca.be>
2023-07-12 11:31:47 +0200

    [fix] camera picture height and width

--- src/app/counter/consumption-counter/consumption-counter.page.ts
+++ src/app/counter/consumption-counter/consumption-counter.page.ts
@@ -43,8 +43,6 @@ export class ConsumptionCounterPage {
       allowEditing: false,
       resultType: CameraResultType.DataUrl,
       source: CameraSource.Camera,
-      width: 500,
-      height: 200
     });
     
     this.image = image.dataUrl;
