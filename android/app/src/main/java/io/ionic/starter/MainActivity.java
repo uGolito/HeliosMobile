e56a498db86f9281a12bad482ef72f7e796b3f7c
Closus <claudio@manca.be>
2023-07-18 12:27:35 +0200

    [remove] javamanifest

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,15 +1,8 @@
 package io.ionic.starter;
 
 import com.getcapacitor.BridgeActivity;
-import com.ahm.capacitor.camera.preview.CameraPreview;
+
 
 public class MainActivity extends BridgeActivity {
-  @override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
 
-    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
-      add(CameraPreview.class);
-    }});
-  }
 }
